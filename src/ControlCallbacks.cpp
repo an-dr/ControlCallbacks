abf73da0ef391e2b604ce3346a37c1cee8e527f6
an-dr <mail@agramakov.me>
2020-03-20 21:28:51 +0100

    added some log

--- src/ControlCallbacks.cpp
+++ src/ControlCallbacks.cpp
@@ -16,6 +16,7 @@ ControlCallbacks::ControlCallbacks(Connection_t *conns, uint32_t con_num)
     : connections(conns), Connections_n(con_num) {}
 
 int ControlCallbacks::Exec(uint32_t cmd) {
+    printf("[ControlCallbacks] got %d\n", cmd);
     for (int i = 0; i < Connections_n; ++i) {
         if (cmd == connections[i].cmd_code) {
             connections[i].func();
