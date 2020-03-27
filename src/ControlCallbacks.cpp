b63725125a2742e988cbff3328568c4470702414
Andrei Gramakov <mail@agramakov.me>
2020-03-27 21:20:57 +0100

    small warning fix

--- src/ControlCallbacks.cpp
+++ src/ControlCallbacks.cpp
@@ -16,8 +16,8 @@ ControlCallbacks::ControlCallbacks(Connection_t *conns, uint32_t con_num)
     : connections(conns), Connections_n(con_num) {}
 
 int ControlCallbacks::Exec(uint32_t cmd) {
-    printf("[ControlCallbacks] got %d\n", cmd);
-    for (int i = 0; i < Connections_n; ++i) {
+    printf("[ControlCallbacks] got %lu\n", cmd);
+    for (uint32_t i = 0; i < Connections_n; ++i) {
         if (cmd == connections[i].cmd_code) {
             connections[i].func();
             return 0;
